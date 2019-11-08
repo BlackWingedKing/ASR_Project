d609a60c5ea777d5cb9a3466f200e5c4ac9766e8
BlackWingedKing <ritesh.goru@gmail.com>
2019-11-08 14:39:37 +0530

    training pipeline done

--- train.py
+++ train.py
@@ -17,10 +17,10 @@ import pandas as pd
 import glob
 import time
 from model_fused import AVNet, VideoNet, AudioNet
-
+from data_loader import DataLoaderTrain
 # parameters and hyper params
 batch_size = 1
-nepochs = 5
+nepochs = 1
 LR = 0.001
 
 #gpu settings
@@ -30,6 +30,28 @@ torch.manual_seed(1)
 device = torch.device("cuda" if use_cuda else "cpu")
 kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 
+ce_loss = nn.CrossEntropyLoss()
+def train(vmodel, amodel, avmodel, optimiser, epochs, train_loader):
+    # train function 
+    # load data from the dataloader
+    loss_list = []
+    it = 0
+    for vid, aus, au in enumerate(train_loader):
+        vid = vid.to(device)
+        aus = aus.to(device)        
+        au = au.to(device)
+        vfeat = vmodel(vid)
+        afeat = amodel(au)
+        asfeat = amodel(aus)
+        p = avmodel(vfeat, afeat)
+        ps = avmodel(vfeat, asfeat)
+        gt = torch.ones_like(p).to(device)
+        # loss = torch.mean(torch.log(p) + torch.log(1-ps))
+        loss = torch.mean(ce_loss(p, gt) + ce_loss((1-ps), gt))
+        loss.backward()
+        loss_list.append(loss.item())
+        print('training ','iteration: ',it,'avg loss: ',loss.item())
+
 def main():
     # initialise the models
     vmodel = VideoNet().to(device)
@@ -37,7 +59,8 @@ def main():
     avmodel = AVNet().to(device)
     params = list(vmodel.parameters())+list(amodel.parameters())+list(avmodel.parameters())
     optimiser = optim.Adam(params,lr=LR)
-    train(vmodel, amodel, avmodel, optimiser)
+    train_loader = DataLoaderTrain()
+    train(vmodel, amodel, avmodel, optimiser, nepochs, train_loader)
 
 if __name__ == '__main__':
     main()    
