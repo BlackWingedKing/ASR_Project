39b6105ccc7e89507deaedb5c08172ab69a0a1bd
Parthasarathi Khirwadkar <kparth98@gmail.com>
2019-11-10 13:26:53 +0530

    storing both audio separately now

--- download_data.py
+++ download_data.py
@@ -22,7 +22,7 @@ def trim_audio(in_path, out_path, start_time, end_time):
     tfm.build(in_path, out_path)
 
 
-def download_video(yt_id, start_time, end_time, fps, aud_sampling, path_vid, path_shift, path_unshift, rng):
+def download_video(yt_id, start_time, end_time, fps, aud_sampling, path_vid, path_snippet, path_shift, path_unshift, rng):
     link = ["https://www.youtube.com/watch?v="+id for id in yt_id]
     vid_num = 0
     for i in range(len(link)):
@@ -42,9 +42,11 @@ def download_video(yt_id, start_time, end_time, fps, aud_sampling, path_vid, pat
             # taking randomly sampled 4.2 sec video from original 10 sec video
             start_time = round(rng.rand()*3.8, 2)
             inp_unshift = ffmpeg.input(filename, loglevel='fatal', ss=start_time, t=4.2)
-            op_unshift, err = ffmpeg.output(inp_unshift,
-                                            path_unshift+'vid_'+str(vid_num+1)+'.mp4').run()
+            op_unshift, err = ffmpeg.output(inp_unshift.video,
+                                            path_snippet+'vid_'+str(vid_num+1)+'.mp4').run()
             # shifting audio
+            trim_audio('data/temp/'+tmp+'_aud_resampled.wav',
+                       path_unshift+'video_'+str(vid_num)+'.wav', start_time, start_time+4.2)
             trim_audio('data/temp/tmp_aud_resampled.wav',
                        path_shift+'video_'+str(vid_num+1)+'.wav', 5.8, 10)
             vid_num += 1
@@ -53,7 +55,7 @@ def download_video(yt_id, start_time, end_time, fps, aud_sampling, path_vid, pat
             print("Connection Error")  # to handle exception
 
 
-def download_yt_link(vid_num, link, start_time, end_time, fps, aud_sampling, path_vid, path_shift,
+def download_yt_link(vid_num, link, start_time, end_time, fps, aud_sampling, path_vid, path_snippet, path_shift,
                      path_unshift, rng):
     try:
         yt = YouTube(link)
@@ -74,9 +76,11 @@ def download_yt_link(vid_num, link, start_time, end_time, fps, aud_sampling, pat
     # taking randomly sampled 4.2 sec video from original 10 sec video
     start_time = round(rng*3.8, 2)
     inp_unshift = ffmpeg.input(filename, loglevel='fatal', ss=start_time, t=4.2)
-    op_unshift, err = ffmpeg.output(inp_unshift,
-                                    path_unshift+'video_'+str(vid_num)+'.mp4').run()
+    op_unshift, err = ffmpeg.output(inp_unshift.video,
+                                    path_snippet+'video_'+str(vid_num)+'.mp4').run()
     # shifting audio
+    trim_audio('data/temp/'+tmp+'_aud_resampled.wav',
+               path_unshift+'video_'+str(vid_num)+'.wav', start_time, start_time+4.2)
     trim_audio('data/temp/'+tmp+'_aud_resampled.wav',
                path_shift+'video_'+str(vid_num)+'.wav', 5.8, 10)
     # clean up
@@ -87,12 +91,12 @@ def download_yt_link(vid_num, link, start_time, end_time, fps, aud_sampling, pat
     print("downloaded video "+str(vid_num))
 
 
-def download_video_parallel(yt_id, start_time, end_time, fps, aud_sampling, path_vid, path_shift,
+def download_video_parallel(yt_id, start_time, end_time, fps, aud_sampling, path_vid, path_snippet, path_shift,
                             path_unshift, rng, njobs):
     link = ["https://www.youtube.com/watch?v="+id for id in yt_id]
     Parallel(n_jobs=njobs, prefer="threads")(delayed(download_yt_link)(
         i+1, link[i], start_time[i], end_time[i], fps,
-        aud_sampling, path_vid, path_shift, path_unshift, rng.rand()) for i in range(len(link)))
+        aud_sampling, path_vid, path_snippet, path_shift, path_unshift, rng.rand()) for i in range(len(link)))
 
 
 def renumber_vid(path,start_from):
@@ -113,15 +117,18 @@ def main():
     audioset_path = 'data/balanced_train_segments.csv'  # path to the list of YouTube videos
     audioset = pd.read_csv(audioset_path, quotechar='"',
                            skipinitialspace=True, skiprows=2)
-    num_videos = 200
-    start_num=200
+    num_videos = 2
+    start_num = 0
+    start_from = len(os.listdir('data/train/full_vid/'))+1
     rng = np.random.RandomState(seed=42)
     download_video_parallel(audioset.iloc[start_num:start_num+num_videos, 0], audioset.iloc[start_num:start_num+num_videos, 1],
-                            audioset.iloc[start_num:start_num+num_videos, 2], 29.97, 22100, 'data/train/full_vid/',
-                            'data/train/shifted/', 'data/train/unshifted/', rng, 15)
-    renumber_vid('data/train/full_vid/',start_from=186)
-    renumber_vid('data/train/shifted/',start_from=186)
-    renumber_vid('data/train/unshifted/',start_from=186)
+                            audioset.iloc[start_num:start_num+num_videos, 2], 29.97, 22100, 'data/train/full_vid/','data/train/snippet/',
+                            'data/train/shifted/', 'data/train/unshifted/', rng, 2)
+
+    renumber_vid('data/train/full_vid/',start_from=start_from)
+    renumber_vid('data/train/snippet/',start_from=start_from)
+    renumber_vid('data/train/shifted/',start_from=start_from)
+    renumber_vid('data/train/unshifted/',start_from=start_from)
 
 
 if __name__ == '__main__':
--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 mkdir -p data
 mkdir -p data/train data/temp
-mkdir -p data/train/full_vid data/train/shifted data/train/unshifted
+mkdir -p data/train/full_vid data/train/shifted data/train/unshifted data/train/snippet
 
-pip install -r requirements.txt
+# pip install -r requirements.txt
 # sudo apt-get install ffmpeg sox
